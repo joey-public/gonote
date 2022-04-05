6bb4ce063bf7910e8ba9968f29dcbe66d71844cf
Joey <joeypublicgit@gmail.com>
2022-04-05 15:02:58 -0700

    Very Broken Good luck trying to fix this mess.

--- src/Nodes/Pens/Pen/HandwritingPen/HandwritingPen.gd
+++ src/Nodes/Pens/Pen/HandwritingPen/HandwritingPen.gd
@@ -1,20 +1,30 @@
 class_name HandwritingPen
 extends Pen
 
-enum DRAW_MODES {POLYLINE,POLYLINE_XFORM,TEXTURE,MULTILINE,LINE,CIRCLE,RECT}
-var drawfuncs:Dictionary = {
-	DRAW_MODES.POLYLINE: funcref(self,"_polyline_draw"),
-	DRAW_MODES.POLYLINE_XFORM: funcref(self,"_xform_polyline_draw"),
-	DRAW_MODES.LINE: funcref(self,"_line_draw"),
-}
+var Stroke:PackedScene = preload("res://src/Nodes/Strokes/HandwritingStroke/Stroke.tscn")
+var active = false
 
-export var draw_mode = DRAW_MODES.POLYLINE_XFORM
-
+onready var cur_stroke = Stroke.instance()
 onready var trail = $trail
+onready var strokes = $Strokes
+
+var draw_xform:Transform2D = Transform2D.IDENTITY
+var data_xform:Transform2D = Transform2D.IDENTITY
+var stroke_data:PoolVector2Array
+var bbox:Rect2
+var id:int
+
+var width:float = 1.5
+var color:Color = Color.black
+var anti_a:bool = true
+var texture:Texture
+
+func _ready():
+	self.strokes.add_child(cur_stroke)
 
 
 func _draw():
-	self.drawfuncs[self.draw_mode].call_func()
+#	self._xform_polyline_draw()
 #	self.draw_set_transform_matrix(Transform2D.IDENTITY)
 	self.draw_circle(get_global_mouse_position(),self.width,self.color)
 #	self.draw_circle(self.data_xform.xform(get_global_mouse_position()),self.width,self.color)
@@ -34,17 +44,20 @@ func _input(event):
 
 func scribble(_delta):
 	if Input.is_action_just_pressed("draw"):
-		self._clear_trail_points()
-		self.bbox.size = Vector2.ZERO
-		self.bbox.position = get_global_mouse_position()
+#		self._clear_trail_points()
+		self.cur_stroke = Stroke.instance()
+		print(self.cur_stroke)
+		self.strokes.add_child(cur_stroke)
+		self.cur_stroke.bbox.size = Vector2.ZERO
+		self.cur_stroke.bbox.position = get_global_mouse_position()
 	elif Input.is_action_pressed("draw"):
 #		var pos:Vector2 = self._prev_mouse_pos.linear_interpolate(get_global_mouse_position(),0.1)
 		var pos:Vector2 = get_global_mouse_position()
-		self.stroke_data.append(pos)
-		self._calc_bbox(pos)
+		self.cur_stroke.stroke_data.append(pos)
+		self.cur_stroke._calc_bbox(pos)
 		self.trail.add_point(pos)
 	elif Input.is_action_just_released("draw"):
-		Globals.draw_stack.push_back(Stroke.new(self))
+#		Globals.draw_stack.push_back(Stroke.new(self))
 		self.stroke_data.resize(0)
 	else:
 		self._remove_trail_point(0)
@@ -62,26 +75,16 @@ func _calc_bbox(pos:Vector2):
 func _get_center(box:Rect2=self.bbox):
 	return box.position+0.5*box.size
 
-func _polyline_draw():
-	if self.stroke_data.size() > 2:
-		if self.width>1:draw_circle(self.stroke_data[0],self.width,self.color)
-		self.draw_polyline(self.stroke_data,self.color,self.width,self.anti_a)
-		if self.width>1:draw_circle(self.stroke_data[self.stroke_data.size()-1],self.width/2,self.color)
-	for stroke in Globals.draw_stack:
-		if stroke.mouse_points.size() >2:
-			if stroke.width>1:self.draw_circle(stroke.mouse_points[0],stroke.width/2,stroke.color)
-			self.draw_polyline(stroke.mouse_points,stroke.color,stroke.width,stroke.anti_a)
-			if stroke.width>1:self.draw_circle(stroke.mouse_points[stroke.mouse_points.size()-1],stroke.width/2,stroke.color)
 
-func _xform_polyline_draw():
-	if self.stroke_data.size() > 2:
-		self._set_draw_xform(self.bbox,self.width)
-		self.draw_set_transform_matrix(self.draw_xform) #draw with the tranform
-		self._set_data_xform(self.bbox,self.width)
-		self.draw_circle(self.data_xform.xform(self.stroke_data[0]),self.width/2,self.color)
-		self.draw_polyline(self.data_xform.xform(self.stroke_data),self.color,self.width,self.anti_a)
-		self.draw_circle(self.data_xform.xform(self.stroke_data[-1]),self.width/2,self.color)
-		self._debug_draw()
+#func _xform_polyline_draw():
+#	if self.stroke_data.size() > 2:
+#		self._set_draw_xform(self.bbox,self.width)
+#		self.draw_set_transform_matrix(self.draw_xform) #draw with the tranform
+#		self._set_data_xform(self.bbox,self.width)
+#		self.draw_circle(self.data_xform.xform(self.stroke_data[0]),self.width/2,self.color)
+#		self.draw_polyline(self.data_xform.xform(self.stroke_data),self.color,self.width,self.anti_a)
+#		self.draw_circle(self.data_xform.xform(self.stroke_data[-1]),self.width/2,self.color)
+#		self._debug_draw()
 #	for stroke in Globals.draw_stack:
 #		if stroke.mouse_points.size() >2:
 #			self._set_draw_xform(stroke.bbox,stroke.width)
@@ -91,18 +94,6 @@ func _xform_polyline_draw():
 #			self.draw_polyline(self.data_xform.xform(stroke.mouse_points),stroke.color,self.width,stroke.anti_a)
 #			self.draw_circle(self.data_xform.xform(stroke.mouse_points[-1]),self.width/2,stroke.color)
 ##			self._debug_draw(stroke.bbox)
-
-#SMOOTH but make app very laggy
-func _line_draw():
-	if len(self.stroke_data) < 2: return 
-	for i in self.stroke_data.size():
-		if i !=0:
-			draw_line(self.stroke_data[i-1],self.stroke_data[i],self.color,self.width,self.anti_a)
-			draw_circle(self.stroke_data[i],self.width/2,self.color)
-#	for stroke in Globals.draw_stack:
-#		if stroke.mouse_points.size() >2:
-#			for i in stroke.mouse_points.size():
-#				if i !=0:draw_line(stroke.mouse_points[i-1],stroke.mouse_points[i],self.color,self.width,self.anti_a)
 
 
 func _set_draw_xform(box:Rect2,width:float):
--- src/Nodes/Pens/Pen/Pen.gd
+++ src/Nodes/Pens/Pen/Pen.gd
@@ -3,18 +3,6 @@
 class_name Pen
 extends Node2D
 
-var draw_xform:Transform2D
-var data_xform:Transform2D
-var stroke_data:PoolVector2Array
-var active = false
-var bbox:Rect2
-var id:int
-
-var width:float = 1.5
-var color:Color = Color.black
-var anti_a:bool = true
-var texture:Texture
-
 
 func _ready():
 	pass
